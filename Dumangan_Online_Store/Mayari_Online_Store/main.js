57c01823e521bb9df0d1af20c0da3a9d242478de
Nicole Anne Guzman <70087357+nclnngzmn@users.noreply.github.com>
2022-05-30 13:03:01 +0800

    Add files via upload

---
+++ Dumangan_Online_Store/Mayari_Online_Store/main.js
@@ -0,0 +1,9 @@
+// First slider
+
+$('.slider-one').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  fade: true,
+  cssEase: 'linear'
+});
---
+++ homepage CSS/HOME/main.js
@@ -0,0 +1,17 @@
+// Scroll Reveal
+ window.sr = ScrollReveal();
+
+ sr.reveal('.animate-left', {
+ 	origin: 'left',
+ 	duration: 1000,
+ 	distance: '25rem',
+ 	delay: 600
+ });
+
+
+ sr.reveal('.animate-right', {
+ 	origin: 'right',
+ 	duration: 1000,
+ 	distance: '25rem',
+ 	delay: 600
+ });
